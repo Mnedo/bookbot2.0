596d17b48ec346ff034bb79913d1814d49fdc61a
Mnedo <basecam@yandex.ru>
2021-07-11 18:09:21 +0300

    timezones

--- GoogleCalendar.py
+++ GoogleCalendar.py
@@ -130,12 +130,10 @@ class GoogleCalendar(object):
             now = datetime.datetime.now(tz=self.tz).isoformat() + 'Z'
             nowd = datetime.datetime.now(tz=self.tz).date().isoformat() + 'T23:59:59.999999Z'
         else:
-            dttm += datetime.timedelta(hours=self.tzn)
             now = (dttm).isoformat() + 'Z'
             nowd = (dttm + datetime.timedelta(hours=23, minutes=59, seconds=59)).isoformat() + 'Z'
             nows = datetime.datetime.strptime(dttm.strftime('%Y-%m-%d'), '%Y-%m-%d').isoformat().split('T')[0]
-            now = nows + 'T{}:00:00.000000Z'.format(
-                str(self.tzn + 6) if self.tzn + 6 > 9 else '0' + str(self.tzn + 6))
+            now = nows + 'T{}:00:00.000000Z'.format(str(self.tzn + 3) if self.tzn + 3 > 9 else '0' + str(self.tzn + 3))
             nowd = nows + 'T{}:00:00.000000Z'.format(
                 str(self.tzn + 20) if self.tzn + 20 < 24 else '23')
         now = ''.join(now.split('+{}:00'.format(str(self.tzn) if self.tzn > 9 else '0' + str(self.tzn))))
