0d4729c73f0ab5c90e34a5754e862da391a4ee61
Mnedo <Basecam@gmail.com>
2021-07-22 23:52:46 +0300

    test commit

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-import datetime
+import os
 
-print(datetime.datetime.strptime('wegg', '%t'))
+import git
+
+
+repo = git.Repo(os.getcwd())
+
+files = repo.git.diff(None, name_only=True)
+for f in files.split('\n'):
+    repo.git.add(f)
+print(files)
+repo.git.commit('-m', 'test commit', author='Mnedo <Basecam@gmail.com>')
