34fbeca78bf5e4ed2d24c0b962210cd801978177
Mnedo <basecam@yandex.ru>
2021-07-27 15:59:54 +0300

    fix db models

--- data/events.py
+++ data/events.py
@@ -9,7 +9,7 @@ class EventRes(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'events'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.user_id"))
     master_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("masters.id"))
     service_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("services.id"))
     has_notified = sqlalchemy.Column(sqlalchemy.BOOLEAN, default=False)
